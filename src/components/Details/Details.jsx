2f921ce10f6afe7d4666a51243c45acadf3bdef9
unknown <ay34641@gmail.com>
2024-05-20 22:38:23 +0530

    errors gone

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,3 +1,36 @@
+// import React from 'react';
+// import PropTypes from 'prop-types'; // Import PropTypes
+// import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+// import { Doughnut } from 'react-chartjs-2';
+
+// import useStyles from './styles';
+// import useTransactions from '../../useTransactions';
+
+// const DetailsCard = ({ title, subheader }) => {
+//   const { total, chartData } = useTransactions(title);
+//   const classes = useStyles();
+
+//   return (
+//     <Card className={title === 'Income' ? classes.income : classes.expense}>
+//       <CardHeader title={title} subheader={subheader} />
+//       <CardContent>
+//         <Typography variant="h5">${total}</Typography>
+//         <Doughnut data={chartData} />
+//       </CardContent>
+//     </Card>
+//   );
+// }
+
+// // Add prop type validation
+// DetailsCard.propTypes = {
+//   title: PropTypes.string.isRequired, // Validate title prop as a required string
+//   subheader: PropTypes.string.isRequired, // Validate subheader prop as a required string
+// };
+
+// export default DetailsCard;
+
+// New Code Subheader hata diya 
+
 import React from 'react';
 import PropTypes from 'prop-types'; // Import PropTypes
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
@@ -6,13 +39,13 @@ import { Doughnut } from 'react-chartjs-2';
 import useStyles from './styles';
 import useTransactions from '../../useTransactions';
 
-const DetailsCard = ({ title, subheader }) => {
+const DetailsCard = ({ title }) => {
   const { total, chartData } = useTransactions(title);
   const classes = useStyles();
 
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
-      <CardHeader title={title} subheader={subheader} />
+      <CardHeader title={title} />
       <CardContent>
         <Typography variant="h5">${total}</Typography>
         <Doughnut data={chartData} />
